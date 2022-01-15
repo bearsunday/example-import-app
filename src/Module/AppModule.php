73231415c8a73d3db89537887511146b818aa95c
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-01-15 22:55:11 +0900

    Request import application

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -6,7 +6,9 @@ namespace MyVendor\MyProject\Module;
 
 use BEAR\Dotenv\Dotenv;
 use BEAR\Package\AbstractAppModule;
+use BEAR\Package\Module\ImportAppModule;
 use BEAR\Package\PackageModule;
+use BEAR\Package\Module\Import\ImportApp;
 
 use function dirname;
 
@@ -15,6 +17,9 @@ class AppModule extends AbstractAppModule
     protected function configure(): void
     {
         (new Dotenv())->load(dirname(__DIR__, 2));
+        $this->install(new ImportAppModule([
+            new ImportApp('foo', 'MyVendor\Weekday', 'prod-app')
+        ]));
         $this->install(new PackageModule());
     }
 }
--- src/Resource/Page/Index.php
+++ src/Resource/Page/Index.php
@@ -5,17 +5,22 @@ declare(strict_types=1);
 namespace MyVendor\MyProject\Resource\Page;
 
 use BEAR\Resource\ResourceObject;
+use BEAR\Sunday\Inject\ResourceInject;
 
 class Index extends ResourceObject
 {
+    use ResourceInject;
+
     /** @var array{greeting: string} */
     public $body;
 
     /** @return static */
     public function onGet(string $name = 'BEAR.Sunday'): static
     {
+        $weekday = $this->resource->get('app://foo/weekday?year=2022&month=1&day=1');
         $this->body = [
             'greeting' => 'Hello ' . $name,
+            'weekday' => $weekday
         ];
 
         return $this;
